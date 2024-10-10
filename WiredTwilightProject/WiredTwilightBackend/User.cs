69a41d7dd740721f3cd77748e49854fb6be9d87b
BNascimento4 <brunodonascimenton099@gmail.com>
2024-10-10 15:16:32 -0300

    Registro e Login

--- WiredTwilightProject/WiredTwilightBackend/User.cs
+++ WiredTwilightProject/WiredTwilightBackend/User.cs
@@ -1,17 +1,40 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using BCrypt.Net;
 
-namespace WiredTwilightBackend;
-
-public class User
+namespace WiredTwilightBackend
 {
-    [Key]
-    public string Id;
-    public string Username;
-    public string Password;
-    public User()
+    public class User
     {
-        this.Id = Guid.NewGuid().ToString();
-        this.Username = string.Empty;
-        this.Password = string.Empty;
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string PasswordHash { get; set; }
+
+        // Senha não deve ser armazenada diretamente
+        [NotMapped]
+        public string Password { get; set; }
+
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public void SetPassword(string password)
+        {
+            this.PasswordHash = BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, this.PasswordHash);
+        }
     }
 }
--- WiredTwilightProject/WiredTwilightBackend/WiredTwilightDbContext.cs
+++ WiredTwilightProject/WiredTwilightBackend/WiredTwilightDbContext.cs
@@ -13,7 +13,7 @@ namespace WiredTwilightBackend
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>()
-          .HasKey(u => u.Id);
+                .HasKey(u => u.Id);
 
             // Configurações adicionais para a entidade User
             modelBuilder.Entity<User>()
@@ -22,10 +22,17 @@ namespace WiredTwilightBackend
                 .HasMaxLength(50); // Define um comprimento máximo
 
             modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired() // Define como obrigatória
+                .HasMaxLength(100); // Define um comprimento máximo
+
+            // Remova ou comente a configuração abaixo, pois 'Password' não está mapeada
+            /*
+            modelBuilder.Entity<User>()
                 .Property(u => u.Password)
                 .IsRequired() // Define como obrigatória
                 .HasMaxLength(100); // Define um comprimento máximo
-                                    // Se necessário, adicione configurações adicionais de mapeamento aqui
+            */
         }
 
         public DbSet<User> Users { get; set; }
